51f907aac152ff6223cfeeaba50c695e197ee700
momeemt <momiimt@gmail.com>
2022-05-01 08:54:09 +0900

    👍 add: implement glfw, gl Exception, and utils

--- src/nagu/glfw.nim
+++ src/nagu/glfw.nim
@@ -1,35 +1,78 @@
 import nimgl/glfw
 from nimgl/opengl import glInit, glEnable, GL_TEXTURE_2D
+from opengl as naguOpengl import OpenGLDefect
 
-proc initGlfw =
-  doAssert glfw.glfwInit()
+type
+  NaguContextObj* = object
+    glfw_initialized: bool
+    gl_initialized: bool
+    window: glfw.GLFWWindow
+
+  NaguContext* = ref NaguContextObj
+    ## Represents GLFW context
+
+  GLFWDefect* = object of Defect
+    ## Base defect type for exceptions for GLFW
+  
+  GLFWInitializeDefect* = object of GLFWDefect
+    ## Raised by failed initializing GLFW
+  
+  WindowCreationDefect* = object of GLFWDefect
+    ## Raised by failed creation GLFW Window
+  
+  OpenGLInitializeDefect* = object of OpenGLDefect
+    ## Raised by failed initializing OpenGL
+ 
+proc init (_: typedesc[NaguContext]): NaguContext =
+  if not glfw.glfwInit():
+    raise newException(GLFWInitializeDefect, "Failed initializing GLFW")
+  result.glfw_initialized = true
   glfw.glfwWindowHint(glfw.GLFWContextVersionMajor, 3)
   glfw.glfwWindowHint(glfw.GLFWContextVersionMinor, 3)
   glfw.glfwWindowHint(glfw.GLFWOpenglForwardCompat, glfw.GLFW_TRUE)
   glfw.glfwWindowHint(glfw.GLFWOpenglProfile, glfw.GLFW_OPENGL_CORE_PROFILE)
   glfw.glfwWindowHint(glfw.GLFWResizable, glfw.GLFW_FALSE)
 
+proc initWindow (width, height: int32, title: string, monitor: glfw.GLFWMonitor, share: glfw.GLFWWindow, icon: bool): glfw.GLFWWindow =
+  result = glfw.glfwCreateWindow(width, height, title, monitor, share, icon)
+  if result == nil:
+    raise newException(WindowCreationDefect, "Failed creation Window")
+
+proc initOpenGL: bool =
+  result = glInit()
+  if not result:
+    raise newException(OpenGLInitializeDefect, "Failed initializing OpenGL Context")
+
+proc attention (window: glfw.GLFWWindow) =
+  glfw.makeContextCurrent(window)
+
 proc keyProc(window: glfw.GLFWWindow, key: int32, scancode: int32, action: int32, mods: int32): void {.cdecl.} =
   if key == GLFWKey(Escape) and action == glfw.GLFWPress:
     glfw.setWindowShouldClose(window, true)
 
-proc setup* (width, height: int32, title: string): glfw.GLFWWindow =
+proc setup* (width: int32 = 500, height: int32 = 500, title: string = "window"): NaguContext =
   ## Initializes OpenGL context and gets GLFW Window.
-  initGlfw()
-  result = glfw.glfwCreateWindow(width, height, title, nil, nil)
-  doAssert result != nil
-  discard result.setKeyCallback(keyProc)
-  result.makeContextCurrent()
-  doAssert glInit()
+  result = NaguContext.init()
+  result.window = initWindow(width, height, title, nil, nil, false)
+  result.window.attention()
+  discard result.window.setKeyCallback(keyProc)
+  result.gl_initialized = initOpenGL()
   glEnable(GL_TEXTURE_2D)
 
-template update* (window: glfw.GLFWWindow, body: untyped) =
-  ## The main-loop in GLFW Window.
-  while not window.windowShouldClose:
-    body
-    glfwPollEvents()
-    window.swapBuffers()
-  window.destroyWindow()
-  glfwTerminate()
+proc isWindowOpen (context: NaguContext): bool =
+  result = not context.window.windowShouldClose
 
-export glfw
+proc destroyWindow (context: NaguContext) =
+  context.window.destroyWindow()
+  glfw.glfwTerminate()
+
+proc pollEventsAndSwapBuffers (context: NaguContext) =
+  glfw.glfwPollEvents()
+  context.window.swapBuffers()
+
+template update* (context: NaguContext, body: untyped) =
+  ## The main-loop in GLFW Window.
+  while context.isWindowOpen:
+    body
+    context.pollEventsAndSwapBuffers()
+  context.destroyWindow()
