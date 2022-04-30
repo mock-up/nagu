d5b8bbaa43cbf1bc60fb70ef0c8246c1a85e5bf9
momeemt <momiimt@gmail.com>
2022-04-30 17:51:30 +0900

    👍 add: nagu/VBO.nim

---
+++ src/nagu/vbo.nim
@@ -0,0 +1,43 @@
+## src/nagu/vbo.nim defines the VBO type and procedures related to its for abstracting OpenGL VBO.
+
+from nimgl/opengl import nil
+from program import ProgramObject
+
+type
+  vboObj [I: static[int]; T] = object
+    id: opengl.GLuint
+    data: array[I, T]
+  
+  vboRef [I: static[int]; T] = ref vboObj[I, T]
+  
+  VBO* = object
+
+proc init [I, T] (_: typedesc[vboRef[I, T]]): vboRef[I, T] =
+  result = vboRef[I, T]()
+  opengl.glGenBuffers(1, result.id.addr)
+
+proc bindArrayBuffer[I, T] (vbo: vboRef[I, T]): vboRef[I, T] =
+  result = vbo
+  opengl.glBindBuffer(opengl.GL_ARRAY_BUFFER, vbo.id)
+
+proc assignArray[I, T] (vbo: vboRef[I, T], data: array[I, T]) =
+  var data = data
+  discard vbo.bindArrayBuffer()
+  opengl.glBufferData(opengl.GL_ARRAY_BUFFER, data.len * sizeof(data[0]), data[0].addr, opengl.GL_STATIC_DRAW)
+
+proc `:=`* [I, T] (vbo: var vboRef[I, T], data: array[I, T]) =
+  ## Assigns `data` to `vbo`.
+  vbo.assignArray(data)
+
+proc make* [I: static[int]; T] (_: typedesc[VBO], data: array[I, T]): vboRef[I, T] =
+  ## Makes vbo and assigns `data` to it.
+  result = vboRef[I, T].init()
+  result.data = data
+  result := result.data
+
+proc correspond* [I] (program: ProgramObject, vbo: vboRef[I, float32], name: string, size: int) =
+  ## Ties `vbo` to `program`.
+  let index = opengl.GLuint(program.nameToIndex[name])
+  opengl.glEnableVertexAttribArray(index)
+  opengl.glBindBuffer(opengl.GL_ARRAY_BUFFER, vbo.id)
+  opengl.glVertexAttribPointer(index, opengl.GLint(size), opengl.EGL_FLOAT, false, 0, nil)
